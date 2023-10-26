1d0d195177e7af9ad9c5a49cc8bb3948dc4d06a4
Paulo Ferreira <paulo.carvalhoferreira@protonmail.com>
2023-10-26 21:26:20 +0100

    liquids

--- my-app/my-project/src/lib/components/site_components/Nav.svelte
+++ my-app/my-project/src/lib/components/site_components/Nav.svelte
@@ -1,8 +1,11 @@
-<nav class="flex justify-between text-white font-mono font-bold">
-    <div class="logo">VoodooChilee</div>
-    <div class="nav flex justify-center space-x-6">
+<nav class="flex justify-between text-white font-mono font-bold mb-8">
+	<div class="logo flex flex-col justify-center items-center gap-2">
+        <h1 class="text-5xl font-os">Paulo Ferreira</h1>
+        <h2 class="text-2xl font-os">FullStack WebDev @ Esad</h2>
+    </div>
+	<!-- <div class="nav flex justify-center space-x-6">
         <a href="#">Music</a>
         <a href="#">Projects</a>
         <a href="#">About</a>
-    </div>
+    </div> -->
 </nav>
--- my-app/my-project/src/routes/+page.svelte
+++ my-app/my-project/src/routes/+page.svelte
@@ -1,24 +1,105 @@
+
+
 <script>
-	import { Canvas } from '@threlte/core';
-	import { World } from '@threlte/rapier';
+	// 	import { Canvas } from '@threlte/core';
+	// import { World } from '@threlte/rapier';
 
-	import Scene from '$lib/components/Scene.svelte';
-</script>
+	// import Scene from '$lib/components/Scene.svelte';
+	import Nav from '$lib/components/site_components/Nav.svelte';
+    import { onDestroy, onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
+  
+    let canvasRef;
+  
+	let mouseX = 0;
+let mouseY = 0;
 
-<div class="canvas-wrapper">
-	<Canvas>
-		<World>
-			<Scene />
-		</World>
-	</Canvas>
-  <div class="controls-wrapper">
-   <h1>Voodoochilee</h1>
-  </div>
-</div>
+function handleMouseMove(event) {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+    // You can now use mouseX and mouseY in your simulation
+}
+    onMount(() => {
+      import('webgl-fluid-enhanced').then(({ default: webGLFluidEnhanced }) => {
+        webGLFluidEnhanced.splats();
+        webGLFluidEnhanced.simulation(canvasRef, {
+          SIM_RESOLUTION: 256,
+          VELOCITY_DISSIPATION: 0.29,
+          COLOR_PALETTE: ["#FF0000", "#FF7F00", "#FFFF00", "#00FF00", "#0000FF", "#4B0082", "#8B00FF"],
+        });
+      });
+    });
+
+	
+	let isWorkShow = false;
+	let isMusicShow = false;
+
+	function showWork() {
+		isWorkShow = !isWorkShow;
+	}
+
+	function showMusic() {
+		isMusicShow = !isMusicShow;
+	}
+
+	
+  </script>
+
+  <canvas on:mousemove={handleMouseMove}  bind:this={canvasRef} class="fixed w-[100vw] h-[100vh]"/>
+
+  <main  class="relative z-10 flex flex-col items-center justify-center h-min" style="position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);"> 
+    <Nav />
+	<div class="grid grid-cols-2 gap-8 justify-center">
+		<div class="col1 flex flex-col items-center">
+			<button on:click={showWork}>
+				<h1 class="font-os text-xl font-bold text-white">Work</h1>
+				
+			</button>
+			{#if isWorkShow}
+				<div class="flex flex-col items-center justify-center space-y-3 mt-3" transition:fade={{ delay: 250, duration: 700 }}>
+					<a href="https://www.portodesignbiennale.pt/">Porto Design Biennale</a>
+					<a href="https://www.bluedesignalliance.pt/">Blue Design Alliance </a>
+				</div>
+			{/if}
+			
+		</div>
+		<div class="col2 flex flex-col items-center">
+			<button on:click={showMusic}>
+				<h1 class="font-os text-xl font-bold text-white">Music</h1>
+			</button>
+			{#if isMusicShow}
+				<div class="flex flex-col items-center justify-center space-y-3 mt-3" transition:fade={{ delay: 250, duration: 700 }}>
+					<a href="https://theblackwizards.bandcamp.com/album/reflections">Reflections</a>
+					<a href="https://theblackwizards.bandcamp.com/album/what-the-fuzz">What the Fuzz!</a>
+					<a href="https://theblackwizards.bandcamp.com/album/lake-of-fire">Lake of Fire</a>
+				</div>
+			{/if}
+		</div>
+	</div>
+  </main>
 
 
-<style>
-	.canvas-wrapper {
+
+    
+
+
+
+  <div>HEllo</div>
+
+
+<style lang="postcss">
+	a {
+		@apply text-white font-os;
+	}
+	p {
+		@apply text-white font-os;
+		
+	}
+
+	button {
+		@apply border-2 border-white rounded-lg px-6 py-3 w-40; 
+	}
+	/* .canvas-wrapper {
 		position: absolute;
 		width: 100%;
 		height: 100%;
@@ -32,5 +113,5 @@
     padding: 0.75rem;
     background-color: #00000088;
     color: white
-  }
+  } */
 </style>
