16325665fab761f9c0906304d52b11875bc21320
Paulo Ferreira <paulo.carvalhoferreira@protonmail.com>
2023-12-22 17:11:20 +0000

    fix vercel error

--- my-app/my-project/src/routes/work/[slug]/+page.svelte
+++ my-app/my-project/src/routes/work/[slug]/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import Seo from '$lib/components/site_components/Seo.svelte';
 	import Swiper from '$lib/components/site_components/Swiper.svelte';
-	import Mobile from './Mobile.svelte';
+	import MobileCard from './MobileCard.svelte';
+	import ProjectCard from './ProjectCard.svelte';
 
 	export let data;
 
@@ -12,8 +13,6 @@
 	let gallery = data.data[0].gallery;
 
 	let slides = 1;
-
-	import Project from './project.svelte';
 </script>
 
 <Seo {pr} />
@@ -21,12 +20,12 @@
 <main class="page_container py-10">
 	<div class="grid grid-cols-1 lg:grid-cols-2">
 		<div class="left hidden lg:block page_container">
-			<Project {pr}></Project>
+			<ProjectCard {pr}></ProjectCard>
 		</div>
 		<div class="right page_container">
 			<Swiper slidesPerView={slides} files={gallery} />
 			<div class="mobile lg:hidden page_container">
-				<Mobile {pr} />
+				<MobileCard {pr} />
 			</div>
 		</div>
 	</div>
